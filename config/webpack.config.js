4223517b767f1cef8ee7b54e727a5110812c461a
bananaeat <qiuyuan463@gmail.com>
2023-04-17 05:10:33 +0100

    Some messy changes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -113,7 +113,7 @@ module.exports = function (webpackEnv) {
         // css is located in `static/css`, use '../../' to locate index.html folder
         // in production `paths.publicUrlOrPath` can be a relative path
         options: paths.publicUrlOrPath.startsWith('.')
-          ? { publicPath: '../../' }
+          ? { publicPath: 'public' }
           : {},
       },
       {
