ad202887c7cceba2c04dc94f0b7fc08a60035219
bananaeat <qiuyuan463@gmail.com>
2023-04-18 00:51:26 +0100

    Added about/help

--- src/WordDict/Menu/Menu.tsx
+++ src/WordDict/Menu/Menu.tsx
@@ -149,14 +149,14 @@ export const Menu : React.FC < Props > = (props : Props) => {
                 : ''}`}>
                 <div className="modal-background"></div>
                 <div className="modal-card">
-                    <header className="modal-card-head">
-                        <p className="modal-card-title">猫猫词库 Word Store v1.0.0</p>
+                    <header className="modal-card-head has-background-black">
+                        <p className="modal-card-title has-text-white">猫猫词库 Word Store v1.0.0</p>
                         <button
                             className="delete"
                             aria-label="close"
                             onClick={() => setAboutVisible(false)}></button>
                     </header>
-                    <section className="modal-card-body has-text-dark markdown">
+                    <section className="modal-card-body has-text-black markdown">
                         <ReactMarkdown>{`
 ## 关于 猫猫词库 / Word Store
 
@@ -170,7 +170,14 @@ export const Menu : React.FC < Props > = (props : Props) => {
 
 * 词汇存储和查询：用户可以自定义创建专业词汇的词条/对应释意/内容说明，并设定Tag分类领域/属性，词条内容支持搜索查询；
 * 语段专业名词提取和释义：用户提供API后，可以使用「AI Tools」工具输入任意语段，此应用可自动提取该语段中包含的专有名词，并为所有名词建立翻译/自动释意词条/标记已有Tag；
-* 从文件到家读取词语列表：支持json格式保存； -支持移动设备和PC网页端，不同设备词汇暂时不互通。
+* 从文件读取词语列表：支持json格式保存； -支持移动设备和PC网页端，不同设备词汇暂时不互通。
+
+# 使用方法
+
+* 添加词语：点击「添加词语」按钮，输入词条名称、释义、点击相关Tag，点击「保存」按钮，即可创建词条；
+* 词语智能提取：点击「AI Tools」按钮，输入API key和语段，点击「提交」按钮，即可提取语段中的专业名词；
+* 文件操作：点击「保存到本地」按钮可下载json文件;点击「读取本地文件」按钮，选择本地json文件，即可导入词汇列表。
+* Tag 管理：点击添加词语->Tag的「+」按钮，可添加Tag；点击「扳手」按钮，可以删除Tag。
                             `}
                         </ReactMarkdown>
                     </section>
